1f8a879f5782b122a5ac418bb89e91ab9613849e
Ramom <ra-mom2000@hotmail.com>
2025-03-06 18:51:20 -0300

    code restructuring and login screen

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { defineConfig } from 'vite'
+import path from 'node:path'
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+import type { InlineConfig } from 'vitest'
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
